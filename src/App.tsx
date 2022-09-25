8891f6f3c7aa81bbe090c6933a514e013664ae6b
aadarsh805 <aadarshthakur80501@gmail.com>
2022-09-25 15:23:15 +0530

    advanced prop types

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import "./App.css";
 import Greet from "./components/Greet";
+import Heading from "./components/Heading";
+import Oscar from "./components/Oscar";
 import Person from "./components/Person";
 import PersonList from "./components/PersonList";
+import Status from "./components/Status";
 
 function App() {
   const personName = {
@@ -26,9 +29,16 @@ function App() {
 
   return (
     <div className="App">
-      <Greet name="Aadarsh" messageCount={20} isLoggedIn={true} />
+      {/* <Greet name="Aadarsh" messageCount={20} isLoggedIn={true} />
       <Person name={personName} />
-      <PersonList names={nameList} />
+      <PersonList names={nameList} /> */}
+
+      {/* <Status status="loading" /> */}
+      <Heading>Placeholder text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Leonardo Decaprio</Heading>
+      </Oscar>
+      <Greet name="Aadarsh"  isLoggedIn={true} />
     </div>
   );
 }
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,15 +1,16 @@
 type GreetProps = {
   name: string;
-  messageCount: number;
+  messageCount?: number;
   isLoggedIn: boolean;
 };
 
 const Greet = (props: GreetProps) => {
+  const { messageCount = 0 } = props;
   return (
     <div>
       <h2>
         {props.isLoggedIn
-          ? ` Yoroshiku! ${props.name} sama, you have ${props.messageCount}
+          ? ` Yoroshiku! ${props.name} sama, you have ${messageCount}
         notifications`
           : `Welcome Guest`}
       </h2>
